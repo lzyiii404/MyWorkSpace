43eb454df07390231ffcc74a24c836c2bde30aa0
jjyy_Huang <809440459@qq.com>
2020-09-15 18:13:03 +0800

    ps_hello_world

---
+++ ZYNQ/ARM/Hello_world/Hello_world.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_hello_world.elf_on_local.tcl
@@ -0,0 +1,15 @@
+connect -url tcp:127.0.0.1:3121
+source D:/MyWorkSpace/ZYNQ/ARM/Hello_world/Hello_world.sdk/design_1_wrapper_hw_platform_0/ps7_init.tcl
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent JTAG-HS1 210512180081"} -index 0
+rst -system
+after 3000
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent JTAG-HS1 210512180081"} -index 0
+loadhw -hw D:/MyWorkSpace/ZYNQ/ARM/Hello_world/Hello_world.sdk/design_1_wrapper_hw_platform_0/system.hdf -mem-ranges [list {0x40000000 0xbfffffff}]
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent JTAG-HS1 210512180081"} -index 0
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent JTAG-HS1 210512180081"} -index 0
+dow D:/MyWorkSpace/ZYNQ/ARM/Hello_world/Hello_world.sdk/hello_world/Debug/hello_world.elf
+configparams force-mem-access 0
+bpadd -addr &main
